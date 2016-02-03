ff882d6dad93b1070eb16d2912cfd8eaf22c7b38
deepirms2619 <deepirms2619@gmail.com>
2016-02-03 13:10:03 +0530

    Create Test.java

---
+++ project1/Test.java
@@ -0,0 +1,16 @@
+package project1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+public class Test {
+
+	public static void main(String args[]) {
+
+		System.out.println("Execution is going on");
+		System.out.println("Finished Execution");
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://google.com");
+	}
+}
